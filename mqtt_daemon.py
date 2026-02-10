00322bb1660034a04c99f926fa44f62f6e389c94
MikroMirza <mirkodjukic314@gmail.com>
2026-02-10 16:46:53 +0100

    [ADD] something something added

--- mqtt_daemon.py
+++ mqtt_daemon.py
@@ -3,6 +3,9 @@ import queue
 import time
 import json
 import paho.mqtt.client as mqtt
+from influx_writer import write_api, bucket
+from influxdb_client import Point
+from influxdb_client.client.write_api import WritePrecision
 
 data_queue = queue.Queue(maxsize=10_000)
 
@@ -19,25 +22,66 @@ class MqttDaemon(threading.Thread):
 
     def run(self):
         batch = []
+        last_flush = time.time()
 
         while not self.stop_event.is_set():
             try:
-                item = data_queue.get(timeout=self.interval)
-                batch.append(item)
+                data = data_queue.get(timeout=0.5)
+                batch.append(data)
 
-                if len(batch) >= self.batch_size:
+                if (
+                    len(batch) >= self.batch_size or
+                    time.time() - last_flush >= self.interval
+                ):
                     self.flush(batch)
                     batch.clear()
+                    last_flush = time.time()
 
             except queue.Empty:
-                if batch:
-                    self.flush(batch)
-                    batch.clear()
+                continue
 
         if batch:
             self.flush(batch)
 
     def flush(self, batch):
-        print("FLUSHED")
-        payload = json.dumps(batch)
-        self.client.publish(self.topic, payload)
+        for data in batch:
+            measurement = data["sensor"]
+
+            tags = {
+                "device": data["sensor_device"],
+                "pi": data["pi"],
+                "simulated": str(data["simulated"])
+            }
+
+            fields = {}
+
+            if "fields" in data:
+                for key in data["fields"]:
+                    value = data.get(key)
+                    if isinstance(value, (int, float)):
+                        fields[key] = value
+            elif "value" in data:
+                v = data["value"]
+                if measurement in ["Button", "Motion sensor"]:
+                    fields["state"] = 1 if v in ["Pressed", "Motion detected"] else 0
+
+                else:
+                    try:
+                        fields["value"] = float(v)
+                    except (ValueError, TypeError):
+                        continue 
+
+            if not fields:
+                continue
+
+            point = Point(measurement)
+
+            for k, v in tags.items():
+                point = point.tag(k, v)
+
+            for k, v in fields.items():
+                point = point.field(k, v)
+
+            point = point.time(data["timestamp"], WritePrecision.NS)
+
+            write_api.write(bucket="iot", record=point)
